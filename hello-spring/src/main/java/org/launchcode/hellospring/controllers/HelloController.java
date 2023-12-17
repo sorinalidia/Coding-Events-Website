46441801a3cb75d39782389efd7dbb6f2808580a
sorinalidia <sorinalidiategla@gmail.com>
2023-12-18 01:06:32 +0200

    added a form submission for greeting

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -31,4 +31,17 @@ public class HelloController {
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
+
+    @GetMapping("form")
+    @ResponseBody
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action='hello'>" +
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
 }
