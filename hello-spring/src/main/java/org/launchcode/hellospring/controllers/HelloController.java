ca8d3ca2a2515af817180367341e41c40d02808e
sorinalidia <sorinalidiategla@gmail.com>
2023-12-18 01:12:31 +0200

    added post request submission

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,10 +1,7 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -20,7 +17,7 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
-    @GetMapping("hello")
+    @RequestMapping(method = {RequestMethod.GET,RequestMethod.POST},value = "hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
@@ -37,7 +34,7 @@ public class HelloController {
     public String helloForm(){
         return "<html>" +
                 "<body>" +
-                "<form action='hello'>" +
+                "<form action='hello' method='post'>" +
                 "<input type='text' name='name'>" +
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
