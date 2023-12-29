a314783103cebcbe60d4f2c724dc77ade4be5405
sorinalidia <sorinalidiategla@gmail.com>
2023-12-29 21:52:39 +0200

    added displaying for validation errors on coding events

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/EventController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/EventController.java
@@ -25,6 +25,7 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(Model model){
         model.addAttribute("title","Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
@@ -33,7 +34,6 @@ public class EventController {
     public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
         if(errors.hasErrors()){
             model.addAttribute("title","Create Event");
-            model.addAttribute("errorMsg","Bad Data!");
             return "events/create";
         }
         EventData.add(newEvent);
