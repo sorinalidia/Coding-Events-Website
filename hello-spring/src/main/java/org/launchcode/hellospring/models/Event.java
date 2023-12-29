64143f0d5647b2ba1312277e7b05bd62f1500e89
sorinalidia <sorinalidiategla@gmail.com>
2023-12-29 20:28:59 +0200

    added validation annotations on coding events

--- hello-spring/src/main/java/org/launchcode/hellospring/models/Event.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/models/Event.java
@@ -1,16 +1,31 @@
 package org.launchcode.hellospring.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.lang.NonNull;
+
 import java.util.Objects;
 
 public class Event {
     private int id;
     private static int nextId = 1;
+
+    @NotBlank
+    @Size(min=3,max=50,message = "Name must have between 3 and 50 characters!")
     private String name;
+
+    @Size(max=500,message = "Description too long!")
     private String description;
 
-    public Event(String name, String description) {
+    @Email(message = "Invalid email. Try again!")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
@@ -33,6 +48,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     @Override
