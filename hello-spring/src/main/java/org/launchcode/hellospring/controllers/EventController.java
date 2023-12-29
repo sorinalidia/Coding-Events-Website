5f644073a135fcccf9d40942f6b415fb794c112e
sorinalidia <sorinalidiategla@gmail.com>
2023-12-29 18:21:53 +0200

    created a model class on coding events

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/EventController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.hellospring.controllers;
 
+import org.launchcode.hellospring.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,23 +14,25 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static List<String> events=new ArrayList<>();
+    private static List<Event> events=new ArrayList<>();
     @GetMapping
     public String displayAllEvents(Model model){
+        model.addAttribute("title","All Events");
         model.addAttribute("events",events);
         return "events/index";
     }
 
     //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model){
+        model.addAttribute("title","Create Event");
         return "events/create";
     }
 
     //lives at /events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+        events.add(new Event(eventName));
         return "redirect:";
     }
 }
---
+++ hello-spring/src/main/java/org/launchcode/hellospring/models/Event.java
@@ -0,0 +1,22 @@
+package org.launchcode.hellospring.models;
+
+public class Event {
+    private String name;
+
+    public Event(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
