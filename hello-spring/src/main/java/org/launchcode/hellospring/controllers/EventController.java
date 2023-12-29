e9adbdae9a773be854ae48f96f2b0b482b2065ce
sorinalidia <sorinalidiategla@gmail.com>
2023-12-29 18:44:45 +0200

    created a model property on coding events

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/EventController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/EventController.java
@@ -31,8 +31,8 @@ public class EventController {
 
     //lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(new Event(eventName));
+    public String createEvent(@RequestParam String eventName,@RequestParam String eventDescription){
+        events.add(new Event(eventName,eventDescription));
         return "redirect:";
     }
 }
