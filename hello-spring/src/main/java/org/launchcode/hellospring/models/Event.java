ffd96e48e3c0d9e4710d3f7eb5df701661bb6eb5
sorinalidia <sorinalidiategla@gmail.com>
2023-12-29 18:52:28 +0200

    added model identifier on coding events

--- hello-spring/src/main/java/org/launchcode/hellospring/models/Event.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/models/Event.java
@@ -1,12 +1,18 @@
 package org.launchcode.hellospring.models;
 
+import java.util.Objects;
+
 public class Event {
+    private int id;
+    private static int nextId = 1;
     private String name;
     private String description;
 
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -15,6 +21,10 @@ public class Event {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getDescription() {
@@ -28,5 +38,18 @@ public class Event {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
