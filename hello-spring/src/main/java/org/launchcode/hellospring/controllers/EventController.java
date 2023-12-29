21c0596912aa9ccdc27668e6fbcb9e3c37f25f9f
sorinalidia <sorinalidiategla@gmail.com>
2023-12-29 20:40:28 +0200

    added handling for validation errors on coding events

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/EventController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/EventController.java
@@ -1,9 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.hellospring.data.EventData;
 import org.launchcode.hellospring.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -28,7 +30,12 @@ public class EventController {
 
     //lives at /events/create
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event newEvent){
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
+        if(errors.hasErrors()){
+            model.addAttribute("title","Create Event");
+            model.addAttribute("errorMsg","Bad Data!");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:";
     }
